11b523acb4c900b48237618a2df6f96b6a475245
outlandkarasu <outland.karasu@gmail.com>
2018-04-04 22:55:27 +0900

    issue #1 fix tsconfig paths.

--- karma.conf.ts
+++ karma.conf.ts
@@ -27,7 +27,7 @@ module.exports = (config: Config): void => {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        "**/*.ts": "karma-typescript"
+        "src/**/*.ts": "karma-typescript"
     },
 
 
