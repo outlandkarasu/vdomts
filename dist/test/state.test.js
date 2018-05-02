0b7e9a0d3906668ca04b41348074ab95ff1c917a
outlandkarasu <outland.karasu@gmail.com>
2018-05-02 18:09:07 +0900

    issue #16 implement state store (#17)
    * issue #16 add action class.
    
    * issue #16 implement store.

---
+++ dist/test/state.test.js
@@ -0,0 +1,54 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var chai_1 = require("chai");
+var store_1 = require("../store");
+describe("create action", function () {
+    var TestAction = (function (_super) {
+        __extends(TestAction, _super);
+        function TestAction() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return TestAction;
+    }(store_1.Action));
+    it("compare same entries", function () {
+        var a = new TestAction({ value: "test" });
+        chai_1.assert.equal(a.param.value, "test");
+    });
+});
+describe("reduce state", function () {
+    var store = new store_1.Store({ value: "initial" });
+    var TestAction = (function (_super) {
+        __extends(TestAction, _super);
+        function TestAction() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return TestAction;
+    }(store_1.Action));
+    var reducer = function (s, a) {
+        s.value = a.param.value;
+        return s;
+    };
+    store.addReducer(TestAction, reducer);
+    chai_1.assert.equal(store.state.value, "initial");
+    chai_1.assert.isTrue(store.doAction(new TestAction({ value: "test" })));
+    chai_1.assert.equal(store.state.value, "test");
+    var TestAction2 = (function (_super) {
+        __extends(TestAction2, _super);
+        function TestAction2() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return TestAction2;
+    }(store_1.Action));
+    chai_1.assert.isFalse(store.doAction(new TestAction2({ value: "test" })));
+});
+//# sourceMappingURL=state.test.js.map
---
+++ src/store.ts
@@ -0,0 +1,114 @@
+/**
+ *  state store implementation.
+ */
+
+/// action type.
+export abstract class Action<P extends Object> {
+
+    /// action parameter.
+    readonly param: Readonly<P>;
+
+    /**
+     *  initialize 
+     *
+     *  @param param parameter object.
+     */
+    constructor(param: P) {
+        // copy parameter values
+        const p: any = {};
+        for(const k of Object.keys(param)) {
+            p[k] = (<any>param)[k];
+        }
+        this.param = <Readonly<P>>p;
+    }
+}
+
+/// action constructor.
+export interface ActionConstructor<A extends Action<P>, P extends Object> {
+    new(param: P): A;
+}
+
+/// reducer function.
+export interface Reducer<S, P extends Object, A extends Action<P>> {
+    (state: S, action: A): S;
+}
+
+/// reducer list entry
+class ReducerEntry {
+
+    /// reducer object.
+    readonly reducer: any;
+
+    /// call reducer function.
+    readonly callReducer: (a: Object) => boolean;
+
+    constructor(reducer: any, callReducer: (a: Object) => boolean) {
+        this.reducer = reducer;
+        this.callReducer = callReducer;
+    }
+}
+
+/// state store.
+export class Store<S> {
+
+    /// state object.
+    private state_: S;
+
+    /// reducer entiries.
+    private reducers_: ReducerEntry[] = [];
+
+    /// get state object.
+    get state(): S {
+        return this.state_;
+    }
+
+    /**
+     *  initialize by initial state.
+     *
+     *  @param state initial state.
+     */
+    constructor(state: S) {
+        this.state_ = state;
+    }
+
+    /**
+     *  add a reducer.
+     *
+     *  @param reducer reducer function.
+     *  @return this object.
+     */
+    addReducer<A extends Action<P>, P extends Object>(ctor: ActionConstructor<A, P>, reducer: Reducer<S, P, A>): this {
+        if(this.reducers_.some(e => e.reducer === reducer)) {
+            return this;
+        }
+
+        // append a new reducer.
+        const callReducer = (a: Object): boolean => {
+            if(a instanceof ctor) {
+                this.state_ = reducer(this.state_, <A>a);
+                return true;
+            }
+            return false;
+        };
+        this.reducers_.push(new ReducerEntry(reducer, callReducer));
+
+        return this;
+    }
+
+    /**
+     *  dispatch action and call matched reducers.
+     *
+     *  @param action dispatching action.
+     *  @return true if called any reducers.
+     */
+    doAction<A extends Action<P>, P extends Object>(action: A): boolean {
+        let called = false;
+        for(const r of this.reducers_) {
+            if(r.callReducer(action)) {
+                called = true;
+            }
+        }
+        return called;
+    }
+}
+
